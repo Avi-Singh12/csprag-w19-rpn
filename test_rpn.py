a37971dc842c7b40810e0c94e7190319041b8f37
Avi Singh <singhavi@umich.edu>
2019-03-10 00:32:32 -0500

    Remove test lines from test_rpn.py

--- test_rpn.py
+++ test_rpn.py
@@ -18,6 +18,3 @@ class TestBasics(unittest.TestCase):
     def test_power(self):
         result = rpn.calculate("2 3 ^")
         self.assertEqual(8, result)
-
-#if __name__ == '__main__':
-#    unittest.main()
